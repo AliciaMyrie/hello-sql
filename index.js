9f3b39393df458bf7e36fa6627eebf0571418c73
Alicia Myrie <aliciamyrie@gmail.com>
2022-07-27 22:32:30 -0400

    added the minimum

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-// import mysql2 library
-const mysql = require("mysql2");
+import express from "express";
 
-// connect to our database
-const db = mysql.createConnection({
-  host: "34.170.135.140",
-  user: "root",
-  password: "BuildingSoFLo",
-  database: "swecc-summer-2022",
-});
+const app = express();
+app.use(express.json());
 
-// run a simple query
-db.query("SELECT * FROM Books_am", (err, results) => {
-  if (err) {
-    console.log(err);
-  }
-  // output the results
-  console.log(results);
-  db.end();
-});
+app.get("/", (req, res) => {});
